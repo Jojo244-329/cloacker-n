f4dc0a6892e64c613cdfa5d0342b19f6ae9fefce
Jojo244-329 <Jojofag244@proton.me>
2025-08-17 20:53:26 -0300

    a

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const port = process.env.PORT || 8080;
+const { port, redisClient } = require('./config');
 
 const app = express();
 
